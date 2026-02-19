cec3c4706cd48dbb9d3d0c5e3f815440d54c7cfb
maxbi03 <max03.bi@pm.me>
2026-02-19 17:30:05 +0100

    Centralisation tailwind

---
+++ theme.js
@@ -0,0 +1,16 @@
+// theme.js
+tailwind.config = {
+  darkMode: "class",
+  theme: {
+    extend: {
+      colors: {
+        "forest-green": "#1B4332",
+        "leaf-green": "#2D6A4F",
+        "background-light": "#f6f8f6",
+        "background-dark": "#102215",
+        "primary": "#13ec49",
+      },
+      fontFamily: { "display": ["Space Grotesk", "sans-serif"] }
+    },
+  },
+};
