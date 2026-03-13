e61a507738df25b03e1add23120c25ffe7698a34
maxbi03 <max03.bi@pm.me>
2026-03-13 16:20:11 +0100

    Ajout contrôle téléphone

--- theme.js
+++ theme.js
@@ -9,7 +9,7 @@ window.tailwind.config = {
         "leaf-green": "#2D6A4F",
         "background-light": "#f6f8f6",
         "background-dark": "#102215",
-        "primary": "#2D6A4F",
+        "primary": "#00b503",
       },
       fontFamily: { display: ["Space Grotesk", "sans-serif"] },
     },
