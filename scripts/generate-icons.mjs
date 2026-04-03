1270bfaa688c1418dca3ef94ec538a646c2d6dc1
P0mme2p1 <gabriel.giivry@gmail.com>
2026-04-03 15:17:29 +0200

    logo+build npm

--- scripts/generate-icons.mjs
+++ scripts/generate-icons.mjs
@@ -1,9 +1,9 @@
 import sharp from 'sharp';
 import fs from 'fs';
 
-const svg = fs.readFileSync('./public/icons/icon.svg');
+const svg = fs.readFileSync('./public/icons/logo.svg');
 
-await sharp(svg).resize(192, 192).png().toFile('./public/icons/icon-192.png');
-await sharp(svg).resize(512, 512).png().toFile('./public/icons/icon-512.png');
+await sharp(svg).resize(192, 192).png().toFile('./public/icons/logo.png');
+await sharp(svg).resize(512, 512).png().toFile('./public/icons/logo.png');
 
 console.log('✅ Icônes générées !');
